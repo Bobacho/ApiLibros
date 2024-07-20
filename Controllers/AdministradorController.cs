1335d90d05f33bcc36326e3047853bde9ae15734
Luciano Kolln <luciano.kolln@unmsm.edu.pe>
2024-07-19 23:28:38 -0500

    Agregando la funcionalidad de administrador

--- Controllers/AdministradorController.cs
+++ Controllers/AdministradorController.cs
@@ -1,14 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
 
-namespace MyApp.Namespace
+using ApiLibros.Repositories;
+using ApiLibros.Models;
+
+namespace ApiLibros.Controllers
 {
+    [EnableCors]
+    [ApiController]
+    [Route("/Api/Admin")]
     public class AdministradorController : Controller
     {
-        // GET: AdministradorController
-        public ActionResult Index()
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly UsuarioRepository _repository;
+
+        public AdministradorController(IHttpContextAccessor contextAccesor, UsuarioRepository repository)
         {
-            return View();
+            _contextAccessor = contextAccesor;
+            _repository = repository;
         }
 
+        [HttpGet("/Api/Admin/is-admin")]
+        public IActionResult IsAdministrador([FromQuery] int id)
+        {
+            return Ok(_repository.IsAdmin(id));
+        }
+        [HttpGet("/Api/Admin/search-id")]
+        public IActionResult GetAdministradorById([FromQuery] int id)
+        {
+            var result = _repository.GetAdministradorById(id);
+            if (result == null)
+            {
+                return NoContent(result);
+            }
+            return Ok(result);
+        }
+        [HttpGet]
+        public IActionResult GetAdministradores()
+        {
+            return Ok(_repository.GetAdministradores());
+        }
+        [HttpPost]
+        public IActionResult InsertAdministrador([FromBody] Administrador request)
+        {
+            return Ok(_repository.InsertAdministrador(request));
+        }
+        [HttpPut]
+        public IActionResult UpdateAdministrador([FromQuery] int id, [FromBody] Administrador request)
+        {
+            if (!_repository.UpdateAdministrador(id, request))
+            {
+                return BadRequest(false);
+            }
+            return Ok(true);
+        }
+        [HttpDelete]
+        public IActionResult DeleteAdministrador([FromQuery] int id)
+        {
+            if (!_repository.DeleteAdministrador(id))
+            {
+                return BadRequest(false);
+            }
+            return Ok(true);
+        }
     }
 }
--- Repositories/UsuarioRepository.cs
+++ Repositories/UsuarioRepository.cs
@@ -42,5 +42,65 @@ namespace ApiLibros.Repositories
         {
             return _context.Usuarios.Where(u => u.NombreUsuario == username && u.Contrasena == password).First();
         }
+
+        public bool IsAdmin(int id)
+        {
+            return _context.Administradors.Where(a => a.IdUsuario == id).Count() > 0;
+        }
+
+        public int InsertAdministrador(Administrador request)
+        {
+            _context.Administradors.Add(request);
+            _context.SaveChanges();
+            return _context.Administradors.OrderBy(a => a.IdAdministrador)
+                .Last().IdAdministrador;
+        }
+
+        public Administrador GetAdministradorById(int id)
+        {
+            return _context.Administradors.Where(a => a.IdAdministrador == id).First();
+        }
+
+        public ICollection<Administrador> GetAdministradores()
+        {
+            return _context.Administradors.ToList();
+        }
+
+        public bool UpdateAdministrador(int id, Administrador request)
+        {
+            try
+            {
+                if (_context.Administradors.Find(id) == null)
+                {
+                    return false;
+                }
+                request.IdAdministrador = id;
+                _context.Administradors.Update(request);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public bool DeleteAdministrador(int id)
+        {
+            try
+            {
+                if (_context.Administradors.Find(id) == null)
+                {
+                    return false;
+                }
+                var entity = _context.Administradors.Find(id);
+                _context.Administradors.Remove(entity);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
     }
 }
